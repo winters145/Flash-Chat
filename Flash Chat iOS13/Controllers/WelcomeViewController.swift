d2261babda618a4a60f4bf70c768afb4309fdc95
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-01 12:54:28 +0100

    Added pod to handle animation of title text.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,24 +7,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = ""
-        var characterIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            characterIndex += 1
-        }
+        
+        titleLabel.text = "⚡️FlashChat"
     }
     
-
 }
